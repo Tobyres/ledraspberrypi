7b6dabb23184deccebba2346b2bf93e437c44a07
user <user@EDV3MAC11.local>
2021-03-19 11:27:21 +0100

    basic ui finished

--- LedRaspberryPi/ViewController.swift
+++ LedRaspberryPi/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+//import CocoaMQTT;
 
 class ViewController: UIViewController {
+
+    //let mqttClient = CocoaMQTT(clientID: "iOS Device", host: "192.168.0.X", port: 1883)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func connectSwitch(_ sender: UISwitch) {
+        if sender.isOn {
+           // mqttClient.connect()
+                }
+                else {
+                   // mqttClient.disconnect()
+                }
+    }
+    @IBAction func displayButton(_ sender: UIButton) {
+        
+        // mqttClient.publish("rpi/senseHat", withString: "$textfield text")
 
+    }
 }
 
